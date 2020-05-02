688a214e79d436c328a5b6c295ad9dc67db2de9c
srydzewski <srydzewski21@amherst.edu>
2020-05-02 11:01:04 -0400

    Deleting the old login stuff and fixing the manifest

--- app/src/main/java/hu/ait/aitpuritytest/data/model/LoggedInUser.kt
+++
@@ -1,9 +0,0 @@
-package hu.ait.aitpuritytest.data.model
-
-/**
- * Data class that captures user information for logged in users retrieved from LoginRepository
- */
-data class LoggedInUser(
-        val userId: String,
-        val displayName: String
-)
---
+++ app/src/main/java/hu/ait/aitpuritytest/ui/LoginActivity.kt
@@ -0,0 +1,13 @@
+package hu.ait.aitpuritytest.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hu.ait.aitpuritytest.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
