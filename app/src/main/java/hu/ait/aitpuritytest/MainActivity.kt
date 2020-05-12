01781855295f9e9e0348ee13e1b1408455c63ef3
srydzewski <srydzewski21@amherst.edu>
2020-05-12 15:37:26 -0400

    Reloads the results screen

--- app/src/main/java/hu/ait/aitpuritytest/MainActivity.kt
+++ app/src/main/java/hu/ait/aitpuritytest/MainActivity.kt
@@ -4,6 +4,8 @@ import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.PagerTitleStrip
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main_content.*
 
@@ -19,6 +21,18 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         viewPager.adapter = MyPagerAdapter(supportFragmentManager)
+        viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+            }
+            override fun onPageSelected(position: Int) {
+                (viewPager.adapter as MyPagerAdapter).notifyDataSetChanged()
+            }
+
+        })
 
         pagerTitleStrip = findViewById(R.id.viewPagerTab)
         pagerTitleStrip.setBackgroundColor(Color.rgb(153,153,204))
--- app/src/main/java/hu/ait/aitpuritytest/MyPagerAdapter.kt
+++ app/src/main/java/hu/ait/aitpuritytest/MyPagerAdapter.kt
@@ -3,6 +3,7 @@ package hu.ait.aitpuritytest
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.PagerAdapter
 
 class MyPagerAdapter(fm: FragmentManager): FragmentPagerAdapter(fm) {
 
@@ -20,6 +21,10 @@ class MyPagerAdapter(fm: FragmentManager): FragmentPagerAdapter(fm) {
 
     override fun getCount(): Int {
         return 2
+    }
+
+    override fun getItemPosition(`object`: Any): Int {
+        return PagerAdapter.POSITION_NONE
     }
 
 }
